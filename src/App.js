ceae1b823c08868a0597bf333e34a783ff02732c
PuZZleDucK <puzzleduck@gmail.com>
2017-11-26 04:28:44 +1100

    create basic mockup of responsive layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react';
+import { Flex, Box } from 'reflexbox';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayWidth: window.innerWidth };
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ displayWidth: window.innerWidth });
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Incident Tracker</h1>
-        </header>
-        <p className="App-intro">
-          The map that tracks incidents... now with React!
-        </p>
-      </div>
-    );
+    const { displayWidth } = this.state;
+    const mobile = (displayWidth <= 500)
+    if(mobile) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <Flex p={2} align='center'>
+              <Box px={2} w={1/8}><img src={logo} className="App-logo" alt="logo" /></Box>
+              <Box px={2} w={7/8} align='left'><h1 align='left' className="App-title">Incident Tracker</h1></Box>
+            </Flex>
+          </header>
+          <Flex className='Mobile' p={2}>
+            <Box className='Map' vertical-align='center' px={2} w={3/3}>
+              <h1>Map</h1>
+              <Box className='ListButton'><h1>Button</h1></Box>
+            </Box>
+          </Flex>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <Flex p={2} align='center'>
+              <Box px={2} w={1/8}><img src={logo} className="App-logo" alt="logo" /></Box>
+              <Box px={2} w={7/8} align='left'><h1 align='left' className="App-title">Incident Tracker</h1></Box>
+            </Flex>
+          </header>
+          <Flex className='Desktop' p={2}>
+            <Box className='List' px={2} w={1/3}><h1>Incident List</h1></Box>
+            <Box className='Map' vertical-align='center' px={2} w={2/3}><h1>Map</h1></Box>
+          </Flex>
+        </div>
+      );
+    }
   }
 }
 
