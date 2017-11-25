a6be5e51e1c4135b4040d15d05e50f47bd0d2bce
PuZZleDucK <puzzleduck@gmail.com>
2017-11-26 06:41:14 +1100

    prototype map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+
 import React, { Component } from 'react';
+import * as ol from 'openlayers';
+import OpenLayers from 'openlayers';
 import { Flex, Box } from 'reflexbox';
+import { interaction, layer, custom, control, Interactions, Overlays, Controls, Map, Layers, Overlay, Util } from "react-openlayers";
+import * as olms from 'ol-mapbox-style';
+
 import logo from './logo.svg';
 import './App.css';
+
+function showPopup(evt) {
+  this.overlayComp.overlay.setPosition(evt.coordinate);
+  var lonlat = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+
+  this.popupComp.setContents(
+    `<p>You clicked here:</p><code> ${lonlat[0]}, ${lonlat[1]}</code>`
+  );
+  this.popupComp.show();
+}
+
+var iconFeature = new ol.Feature(new ol.geom.Point([-37.643, 144.928]));
+var source = new ol.source.Vector({features: [iconFeature]});
+var marker = new custom.style.MarkerStyle(
+  'https://openlayers.org/en/v4.0.1/examples/data/icon.png'
+);
 
 class App extends Component {
   constructor() {
@@ -23,7 +45,8 @@ class App extends Component {
 
   render() {
     const { displayWidth } = this.state;
-    const mobile = (displayWidth <= 500)
+    const mobile = (displayWidth <= 500);
+    const middle_of_map = [-37.643, 144.928];
     if(mobile) {
       return (
         <div className="App">
@@ -35,7 +58,11 @@ class App extends Component {
           </header>
           <Flex className='Mobile' p={2}>
             <Box className='Map' vertical-align='center' px={2} w={3/3}>
-              <h1>Map</h1>
+            <Map className='MapView' view={{center: [144.928, -37.643], zoom: 4}} onClick={showPopup}>
+              <Layers>
+                <layer.Tile />
+              </Layers>
+            </Map>
               <Box className='ListButton'><h1>Button</h1></Box>
             </Box>
           </Flex>
@@ -51,8 +78,20 @@ class App extends Component {
             </Flex>
           </header>
           <Flex className='Desktop' p={2}>
-            <Box className='List' px={2} w={1/3}><h1>Incident List</h1></Box>
-            <Box className='Map' vertical-align='center' px={2} w={2/3}><h1>Map</h1></Box>
+            <Box className='List' px={2} w={1/3}>
+              <ul>
+                <li><h2>Incident 1</h2></li>
+                <li><h2>Incident 2</h2></li>
+                <li><h2>Incident 3</h2></li>
+              </ul>
+            </Box>
+            <Box className='Map' px={2} w={2/3}>
+              <Map className='MapView' view={{center: [144.928, -37.643], zoom: 4}} onClick={showPopup}>
+                <Layers>
+                  <layer.Tile />
+                </Layers>
+              </Map>
+            </Box>
           </Flex>
         </div>
       );
